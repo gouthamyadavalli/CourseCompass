adb236f047932ff930f4d20b0baa6cf90df68bf3
gouthamyadavalli <yadavalli.s@northeastern.edu>
2024-09-28 18:40:43 -0400

    ai21

--- src/play.py
+++ src/play.py
@@ -1,5 +1,5 @@
 from ai21 import AI21Client
-from ai21.chat import ChatMessage
+# from ai21.chat import ChatMessage
 
 # Load api key from .env file
 import os
@@ -21,5 +21,29 @@ def upload_file(file_path):
     print(response)
     return response
 
+def query_library(query, labels=None, path=None):
+    response = client.library.answer.create(
+        question=query,
+        path=path,
+        labels=labels,
+        model="text-qa",
+    )
+    print(response)
 
+def get_files():
+    # Just return the first 10 files
+    response = client.library.files.list(
+        offset=0,
+        limit=10,
+        status="PROCESSED" # Apply 
+    )
+    print(response)
+
+# Filter the question to documents with the case-sensitive label "hr"
+query_library("Hello How are you?")
+# print(get_files())
+
+
+
+# upload_file("data/course_comments_text/10685.txt")
 
